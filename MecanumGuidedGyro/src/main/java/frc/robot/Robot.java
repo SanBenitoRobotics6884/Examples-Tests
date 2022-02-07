f62b8b8692e6895aaa4f7462b4a468b2720d9ca9
Nick Carpenedo <nicholas.carpenedo@gmail.com>
2022-02-06 20:48:55 -0800

    Add gyro object

--- MecanumGuidedGyro/src/main/java/frc/robot/Robot.java
+++ MecanumGuidedGyro/src/main/java/frc/robot/Robot.java
@@ -1,106 +1,69 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private static double kMaxSpeed = 0.4;
+
+  private Joystick m_stick;
+  private CANSparkMax m_lFront;
+  private CANSparkMax m_lRear;
+  private CANSparkMax m_rFront;
+  private CANSparkMax m_rRear;
+  private MecanumDrive m_drive;
+  private ADIS16448_IMU m_gyro;
+
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    // initialize SPARK MAX
+    m_lFront = new CANSparkMax(1, MotorType.kBrushless);
+    m_lRear = new CANSparkMax(2, MotorType.kBrushless);
+    m_rFront = new CANSparkMax(3, MotorType.kBrushless);
+    m_rRear = new CANSparkMax(4, MotorType.kBrushless);
+    
+    m_lFront.restoreFactoryDefaults();
+    m_lRear.restoreFactoryDefaults();
+    m_rFront.restoreFactoryDefaults();
+    m_rRear.restoreFactoryDefaults();
+
+    m_rFront.setInverted(true);
+    m_rRear.setInverted(true);
+
+    m_stick = new Joystick(0);
+    m_drive = new MecanumDrive(m_lFront, m_lRear, m_rFront, m_rRear);
+
+    m_gyro = new ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, SPI.Port.kMXP, ADIS16448_IMU.CalibrationTime._1s);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+  public void robotPeriodic() {
+    SmartDashboard.putData("Drive", m_drive);
+    SmartDashboard.putData("Gyro", m_gyro);
+    SmartDashboard.putNumber("Z Heading", m_gyro.getGyroAngleZ());
+    SmartDashboard.putNumber("Z Rate", m_gyro.getGyroRateZ());
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+  public void teleopPeriodic() {
+    double forw = m_stick.getRawAxis(1) * kMaxSpeed;
+    double strafe = m_stick.getRawAxis(0) * kMaxSpeed;
+    double rot = m_stick.getRawAxis(4) * kMaxSpeed;
+    m_drive.driveCartesian(forw, strafe, rot);
   }
-
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
